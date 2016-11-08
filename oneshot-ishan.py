5aae36b6537223877f45dbafdbc818d8e4c61fd0
ishansd <ishan.sd@gmail.com>
2016-11-08 08:30:10 -0600

    Added multiple read write heads

--- oneshot-ishan.py
+++ oneshot-ishan.py
@@ -19,10 +19,11 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
 
 image_size = 28
 input_size = image_size*image_size
-key_size = 50
-num_rows = 50
-weight_decay = 0.8
+key_size = 40
+num_rows = 100
+weight_decay = 0.95
 num_classes = 10
+num_read_heads = 4
 
 shuffle_vector = range(num_classes)
 
@@ -60,47 +61,52 @@ y_ = tf.placeholder(tf.float32,shape=[1,num_classes]);
 memory_module = tf.placeholder(tf.float32,[num_rows, key_size])
 
 #################################
-output_size = 2*key_size + 1 # one output for gate parameter
+output_size = 2*num_read_heads*key_size  + num_read_heads # one output for gate parameter
 
 combined_input = tf.concat(1,[x,prev_output])
 # Controller - 2 layer feed forward network
 x1 = linear_layer(combined_input,input_size+num_classes,500)
-x2 = linear_layer(x1,500,200)
-x3 = linear_layer(x2,200,output_size)
+#x2 = linear_layer(x1,500,200)
+x3 = linear_layer(x1,500,output_size)
 
-read_key = tf.slice(x3,[0,0],[1,key_size])
-write_key = tf.slice(x3,[0,key_size],[1,key_size])
-alpha = tf.slice(x3,[0,2*key_size],[1,1]) # gate parameter
+read_keys = tf.slice(x3,[0,0],[1,num_read_heads*key_size])
+write_key = tf.reshape(tf.slice(x3,[0,num_read_heads*key_size],[1,num_read_heads*key_size]),
+                                                            shape =[num_read_heads,key_size])
 
-key_norm = tf.nn.l2_normalize(read_key,1)
+alpha = tf.reshape(tf.slice(x3,[0,2*num_read_heads*key_size],[1,num_read_heads]),
+                                            shape=[num_read_heads,1]) # gate parameter
 
+keys = tf.reshape(read_keys,shape=[num_read_heads,key_size])
+key_norm = tf.nn.l2_normalize(keys,1)
 
 ######################################################################
 # least used policy to be implemented
-previous_read_weights = tf.placeholder(tf.float32,shape=[num_rows,1])
-least_used_weights = tf.placeholder(tf.float32,shape=[num_rows,1])
+previous_read_weights = tf.placeholder(tf.float32,shape=[num_read_heads,num_rows])
+least_used_weights = tf.placeholder(tf.float32,shape=[num_read_heads,num_rows])
 
 
 beta = tf.sigmoid(alpha)
 write_weights = previous_read_weights*beta + least_used_weights*(1-beta)
 
-previous_usage_weights = tf.placeholder(tf.float32,shape=[num_rows,1])
+previous_usage_weights = tf.placeholder(tf.float32,shape=[num_read_heads,num_rows])
 
 memory_erase = tf.placeholder(tf.float32,shape=[num_rows,1])
 memory = tf.add(tf.mul(memory_erase,memory_module),
-                                        tf.matmul(write_weights,write_key))
+                                        tf.matmul(tf.transpose(write_weights),write_key))
 
 
-mat_norm = tf.nn.l2_normalize(memory,1)
+mat_norm = tf.nn.l2_normalize(memory_module,1)
 similarity = tf.matmul(mat_norm, tf.transpose(key_norm))
-read_weights = tf.transpose(tf.nn.softmax(tf.transpose(similarity)))
-data_read = tf.matmul(tf.transpose(read_weights),memory)
+read_weights = (tf.nn.softmax(tf.transpose(similarity)))
+data_read = tf.matmul((read_weights),memory)
 
-input_to_softmax = linear_layer(tf.concat(1,[data_read, x2]),key_size+200,100)
-y = only_linear_layer(input_to_softmax,100,num_classes)
+flattened_data_read = tf.reshape(data_read,shape=[1,num_read_heads*key_size])
 
-#input_to_softmax = (tf.concat(1,[data_read, x2]))
-#y = only_linear_layer(input_to_softmax,key_size+200,num_classes)
+#input_to_softmax = linear_layer(tf.concat(1,[data_read, x2]),key_size+200,100)
+#y = only_linear_layer(input_to_softmax,100,num_classes)
+
+input_to_softmax = (tf.concat(1,[flattened_data_read, x1]))
+y = only_linear_layer(input_to_softmax,num_read_heads*key_size+500,num_classes)
 
 usage_weights = (weight_decay*previous_usage_weights) + (
                                                 read_weights + write_weights)
@@ -117,20 +123,18 @@ sess = tf.Session()
 saver = tf.train.Saver()
 sess.run(tf.initialize_all_variables())
 
-initial_usage_weights = tf.truncated_normal([num_rows,1],stddev=0.1)
+initial_usage_weights = tf.truncated_normal([num_read_heads,num_rows],stddev=0.1)
 stored_usage_weights = sess.run(initial_usage_weights)
 
-initial_read_weights = tf.truncated_normal([num_rows,1],stddev=0.1)
+initial_read_weights = tf.truncated_normal([num_read_heads,num_rows],stddev=0.1)
 stored_read_weights = sess.run(initial_read_weights)
 
-initial_memory = tf.truncated_normal([num_rows, key_size],stddev=0.1)
+initial_memory = tf.truncated_normal([num_rows, key_size],stddev=0.5)
 stored_memory = sess.run(initial_memory)
 
 stored_least_used_weights = sess.run(initial_usage_weights)
 memory_erase_vector = np.ones([num_rows,1])
 
-initial_error = tf.constant(1.0,shape=[1])
-current_error = sess.run(initial_error)
 
 batch_xs1, batch_ys1 = mnist.train.next_batch(1)
 output = np.zeros([1,num_classes])
@@ -140,9 +144,8 @@ avg_acc = 0
 num_episodes = 50000
 for episode in range(num_episodes):
     random.shuffle(shuffle_vector)
-    print episode
     if episode%500 == 0:
-        save_path=saver.save(sess,"model-"+str(episode)+".ckpt")
+        save_path=saver.save(sess,"model-"+str(episode)+"-.ckpt")
 
     stored_usage_weights = sess.run(initial_usage_weights)
     stored_read_weights = sess.run(initial_read_weights)
@@ -151,23 +154,27 @@ for episode in range(num_episodes):
     memory_erase_vector = np.ones([num_rows,1])
 
 
-
     accuracy_vector = np.zeros((num_classes,1))
     count_vector = np.zeros((num_classes,1))
 
-    rotation = np.random.randint(-15,15,100)
-    for i in range(100):
+    rotation = np.random.randint(-10,10,num_classes*10)
+    global_rotation = np.random.randint(-45,45,num_classes)
+
+    for i in range(num_classes*10):
         in_x, original_class = mnist.train.next_batch(1)
 
-        batch_xs = np.reshape(ndimage.rotate(np.reshape(in_x,[image_size,image_size]), rotation[i], reshape=False),[1,input_size])
+        batch_xs = np.reshape(ndimage.rotate(
+                            np.reshape(in_x,[image_size,image_size]), global_rotation[original_class[0]] + rotation[i],
+                                            reshape=False),[1,input_size])
 
         count_vector[original_class[0]] = count_vector[original_class[0]]+1
         in_y = shuffle_vector[original_class[0]]
+
         batch_ys = np.zeros([1,num_classes])
         batch_ys[0,in_y] = 1
 
         output, stored_read_weights,stored_usage_weights,stored_memory, step, acc = sess.run(
-                                        [y_, read_weights, usage_weights, memory, train_step, accuracy],
+                [y_, read_weights, usage_weights, memory, train_step, accuracy],
                                         feed_dict=
                                         {x: batch_xs, y_: batch_ys,
                                         prev_output: output,
@@ -177,40 +184,44 @@ for episode in range(num_episodes):
                                         previous_usage_weights: stored_usage_weights,
                                         previous_read_weights: stored_read_weights})
 
-        nthsmallest = nsmall(np.reshape(stored_usage_weights,[num_rows]),i+1)
-        ind1 = stored_usage_weights > nthsmallest
-        ind2 = stored_usage_weights <= nthsmallest
-        stored_least_used_weights[ind1]=0
-        stored_least_used_weights[ind2]=1
+
+        min_weight_index = np.argmin(stored_usage_weights,1)
+        stored_least_used_weights = np.zeros([num_read_heads,num_rows])
         memory_erase_vector = np.ones([num_rows,1])
-        memory_erase_vector[np.argmin(stored_usage_weights)]=0
+        for i in range(num_read_heads):
+            stored_least_used_weights[i,min_weight_index[i]] = 1
+            memory_erase_vector[min_weight_index[i]]=0
+
 
         accuracy_vector[original_class[0]] = accuracy_vector[original_class[0]] + acc
 
         avg_acc = avg_acc + acc
-#    if episode%10 == 0:
-#        print (accuracy_vector/count_vector)
-    print avg_acc/(episode+1)
+
+    if episode%100 == 0:
+        print "Episode " + str(episode) + " : "
+        print np.concatenate((accuracy_vector,count_vector),1)
+
 print avg_acc/num_episodes
 
-saver.restore(sess,'model-900.ckpt')
+#saver.restore(sess,'model-900.ckpt')
 stored_usage_weights = sess.run(initial_usage_weights)
 stored_read_weights = sess.run(initial_read_weights)
 stored_memory = sess.run(initial_memory)
 stored_least_used_weights = sess.run(initial_usage_weights)
 memory_erase_vector = np.ones([num_rows,1])
-total_acc = np.zeros(500)
 
 output = np.zeros([1,num_classes])
+accuracy_vector = np.zeros((num_classes,1))
+count_vector = np.zeros((num_classes,1))
 for i in range(500):
 
     batch_xs, in_y = mnist.test.next_batch(1)
-#    in_y = shuffle_vector[in_y[0]]
+    in_y = shuffle_vector[in_y[0]]
     batch_ys = np.zeros([1,num_classes])
     batch_ys[0,in_y] = 1
 
     output, stored_read_weights,stored_usage_weights,stored_memory, acc = sess.run(
-                                    [y, read_weights, usage_weights, memory, accuracy],
+                                    [y_, read_weights, usage_weights, memory, accuracy],
                                     feed_dict=
                                     {x: batch_xs, y_: batch_ys,
                                     prev_output: output,
@@ -220,14 +231,20 @@ for i in range(500):
                                     previous_usage_weights: stored_usage_weights,
                                     previous_read_weights: stored_read_weights})
 
-    nthsmallest = nsmall(np.reshape(stored_usage_weights,[num_rows]),i+1)
-    ind1 = stored_usage_weights > nthsmallest
-    ind2 = stored_usage_weights <= nthsmallest
-    stored_least_used_weights[ind1]=0
-    stored_least_used_weights[ind2]=1
+    min_weight_index = np.argmin(stored_usage_weights,1)
+    stored_least_used_weights = np.zeros([num_read_heads,num_rows])
     memory_erase_vector = np.ones([num_rows,1])
-    memory_erase_vector[np.argmin(stored_usage_weights)]=0
+    for i in range(num_read_heads):
+        stored_least_used_weights[i,min_weight_index[i]] = 1
+        memory_erase_vector[min_weight_index[i]]=0
+
+    count_vector[in_y] = count_vector[in_y]+1
+    accuracy_vector[in_y] = accuracy_vector[in_y] + acc
+
     total_acc[i] = acc
+
+print "Testing accuracy :"
+print np.concatenate((accuracy_vector,count_vector),1)
 
 sess.close()
 print np.sum(total_acc)
