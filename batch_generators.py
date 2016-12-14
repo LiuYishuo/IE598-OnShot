a3cd2321f9eb7df0f9c3d6400ddf867ad6381472
ishansd <ishan.sd@gmail.com>
2016-12-14 16:38:59 -0600

    Added a few comments

--- batch_generators.py
+++ batch_generators.py
@@ -7,27 +7,33 @@ Created on Tue Nov 22 15:27:24 2016
 
 import os
 import numpy as np
-from mann.utils.generators import OmniglotGenerator
 import time
 import multiprocessing as mp
-
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
 
 def f(batch_num):
-   g = OmniglotGenerator(data_folder='/media/konik/Misc/OneShot/data/omniglot/images_background/', \
-   batch_size=16, nb_samples=5, nb_samples_per_class=10, max_rotation=-np.pi/6.0, \
-   max_shift=0.01, max_iter=None)
-   _, (eg_input, eg_output) = g.next()
-   np.savez("newbatch"+str(batch_num), ip = eg_input, op = eg_output)
+    count = 0
+    eg_input = []
+    eg_output = []
+    while (count < 50):
+        x, y = mnist.train.next_batch(1)
+        if y < 5:
+            count += 1
+            eg_input.append(x)
+            eg_output.append(y)
+    print eg_input.shape
+    np.savez("newbatch"+str(batch_num), ip = eg_input, op = eg_output)
 
 
 
 if __name__ == '__main__':
     t1 = time.time()
     try:
-        nprocesses = 100
+        nprocesses = 1
         pool = mp.Pool(nprocesses)
 
-        result = pool.map_async(f, np.arange(6000))
+        result = pool.map_async(f, np.arange(1))
         pool.close()
         pool.join()
         result.get()
@@ -60,5 +66,3 @@ if __name__ == '__main__':
 #    op = data['op']
 #
 #print("Batch loading took %0.5fs" % (time.time() - t1))
-
-
